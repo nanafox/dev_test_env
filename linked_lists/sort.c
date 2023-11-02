9b2874c7cdbb3b6b3bec7cc0b99e03f0d83f22fd
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-11-02 17:31:52 +0000

    Added a function to sort a linked list

---
+++ linked_lists/sort.c
@@ -0,0 +1,41 @@
+#include "node.h"
+
+/**
+ * sort - sort a linked list (uses the selection sort algorithm)
+ * @head: pointer to the head node
+ * @compare: a function pointer to compare data value for swapping
+ *
+ * Return: a pointer to the head (now sorted list), or NULL if list is empty
+ */
+Node *sort(Node **head, int (*compare)(int x, int y))
+{
+	Node *x, *y;
+	int tmp;
+	
+	if (*head == NULL || head == NULL)
+	{
+		return (NULL); /* list is empty */
+	}
+
+	x = *head;
+
+	while (x != NULL)
+	{
+		y = x->next;
+
+		while (y != NULL)
+		{
+			if (compare(y->data, x->data) > 0)
+			{
+				tmp = x->data;
+				x->data = y->data;
+				y->data = tmp;
+			}
+			y = y->next;
+		}
+		x = x->next;
+	}
+
+
+	return (*head);
+}
