10f2df55cf6a3ee23a2db227ae7276e9f91ff5cc
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-11-06 10:05:31 +0000

    Improved _getenv function for better error handling

--- simple_shell/env_handlers.c
+++ simple_shell/env_handlers.c
@@ -17,8 +17,10 @@ char *_getenv(const char *name)
 		if (_strspn(environ[i], name))
 		{
 			if (_strstr(environ[i], name))
+			{
 				/* move past the equal to sign and return the actual value */
 				return ((_strchr(environ[i], '=')) + 1);
+			}
 		}
 	}
 
@@ -52,32 +54,27 @@ path_t *build_path(path_t **head)
 
 	path_value = _getenv("PATH");
 	if (path_value == NULL || *path_value == '\0')
-		return (NULL);
+		return (NULL); /* PATH is not set */
 
 	pathnames = _strtok(path_value, ":");
 	while (pathnames[i] != NULL)
 	{
 		new_node = malloc(sizeof(path_t));
 		if (new_node == NULL)
-		{
 			return (NULL);
-		}
 
 		new_node->pathname = _strdup(pathnames[i]);
 		_strcpy(new_node->pathname, pathnames[i]);
 		new_node->next = NULL;
 
 		if (*head == NULL)
-		{
 			*head = new_node;
-		}
 		else
 		{
 			tail = *head;
 			while (tail->next != NULL)
-			{
 				tail = tail->next;
-			}
+
 			tail->next = new_node;
 		}
 		safe_free(pathnames[i]);
@@ -86,4 +83,20 @@ path_t *build_path(path_t **head)
 
 	free_str(pathnames);
 	return (*head);
+}
+
+/**
+ * print_path - prints the contents in the PATH variable
+ * @list: list of pathnames
+ */
+void print_path(path_t *list)
+{
+	if (list == NULL)
+		return;
+
+	while (list != NULL)
+	{
+		printf("%s\n", list->pathname);
+		list = list->next;
+	}
 }
